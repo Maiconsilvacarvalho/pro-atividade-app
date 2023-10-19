94a194992e285b6f2479be1b1c68b43fa94939f6
Maiconsilvacarvalho <maiquinhomaicon@hotmail.com>
2023-10-19 13:55:26 -0300

    att

--- src/App.js
+++ src/App.js
@@ -1,29 +1,50 @@
-import { Fragment } from 'react';
+import { Fragment, useState } from 'react';
 import './App.css';
 
+let initialState = [
+  {
+    id: 1,
+    descricao: 'Primeira Atividade',
+  },
+  {
+    id: 2,
+    descricao: 'Segunda Atividade',
+  }
+]
+
+
 function App() {
-const atividades = [
-{
-  id: 1,
-  descricao: 'Primeira Atividade',
-},
-{
-  id: 2,
-  descricao: 'Segunda Atividade',
+  const [atividades, setAtividades] = useState(initialState)
+
+
+
+function AddAtividade(e) {
+  e.preventDefault();
+  const atividade = {
+    id: document.getElementById('id').value,
+    descricao: document.getElementById('descricao').value,
+  };
+    atividades.push(atividade)
+    console.log(atividades);
+
+    setAtividades(atividades)
+
 }
-];
+
   return (
     <Fragment>
     <form>
         <input id='id' type='text' placeholder='id'/>
         <input id='descricao' type='text' placeholder='descricao'/>
-        <button>+ ATIVIDADE</button>
+        <button onClick={AddAtividade}>+ ATIVIDADE</button>
     </form>
     <div className="mt-3">
         
           {/* Map é como se fosse forEach */}
-          <ul className="list-group">{atividades.map(ativ =>(
-            <li key={ativ.id} className='list-group-item'>{ativ.id} - {ativ.descricao}</li>
+          <ul className="list-group">
+            {atividades.map(ativ =>(
+            <li key={ativ.id} className='list-group-item'>
+              {ativ.id} - {ativ.descricao}</li>
           ))}</ul>
           
         
